e77adb99f6bfc4bc228e77b59c2089f4d9b255fd
Luis Alfredo Hernandez <lhernandez@flexus.local>
2024-07-25 07:36:14 -0400

    deleting unused package

--- devFestFrontend/src/App.jsx
+++ devFestFrontend/src/App.jsx
@@ -1,22 +1,27 @@
-import { useEffect } from 'react'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
   useEffect(() => {
-    const socket = new WebSocket('wss://ws.bitmex.com/realtime?subscribe=instrument,orderBookL2_25:XBTUSD');
-    socket.onopen = () => console.log('Conectado al servidor de WebSocket');
+    const socket = new WebSocket(
+      "wss://ws.bitmex.com/realtime?subscribe=instrument,orderBookL2_25:XBT"
+    );
+    socket.onopen = () => console.log("Conectado al servidor de WebSocket");
 
-    // socket.onmessage = ({data}) => {
-    //   console.log('Mensaje recibido:', data);
-    // }
+    socket.onmessage = function (response) {
+      const parsedResponse = JSON.parse(response.data);
+      "table" in parsedResponse
+        ? console.log("Received data:", parsedResponse)
+        : null;
 
-   }, []);
-  
-  return (
-    <>
-      Holaa!!
-    </>
-  )
+      setTimeout(() => {
+        console.log("Cerrando la conexión WebSocket después de 5 segundos.");
+        socket.close();
+      }, 2000);
+    };
+  }, []);
+
+  return <>Holaa!!</>;
 }
 
-export default App
+export default App;
